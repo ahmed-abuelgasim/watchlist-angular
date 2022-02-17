6be17f4986203dc64af2dbb5a2e9b382e6d9d39c
Ahmed Abuelgasim <ahmed@potatolondon.com>
2022-02-17 12:39:07 +0000

    Added preliminary routes and configured routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { VideoSourcesService } from './services/video-sources.service';
+
+import { ListViewComponent } from './list-view/list-view.component';
+import { ListsViewComponent } from './lists-view/lists-view.component';
+import { SettingsViewComponent } from './settings-view/settings-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListsViewComponent,
+    SettingsViewComponent,
+    ListViewComponent,
   ],
   imports: [
     BrowserModule,
